679ba33a204ae923f5bac489dfcab89442a9d3df
Jefferson Ramos <jhr.developer@gmail.com>
2019-03-31 22:26:07 -0300

    create on clique to key

--- src/Key/Key.jsx
+++ src/Key/Key.jsx
@@ -4,14 +4,26 @@ import PropTypes from "prop-types";
 
 export default class Key extends Component {
     static propTypes = {
-        text: PropTypes.string.isRequired
+        text: PropTypes.string.isRequired,
+        onClick: PropTypes.func.isRequired,
+        value: PropTypes.string.isRequired,
     };
+
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        const { onClick, value } = this.props;
+        onClick(value);
+    }
 
     render() {
         const { text } = this.props;
 
         return (
-            <button>{text}</button>
+            <button onClick={this.handleClick}>{text}</button>
         );
     }
 }
--- src/Key/__tests__/key.spec.js
+++ src/Key/__tests__/key.spec.js
@@ -1,18 +1,39 @@
 import React from "react";
-import context, { set } from "jest-plugin-context";
+import context from "jest-plugin-context";
+import sinon from "sinon";
 import { shallow } from "enzyme";
 
 import Key from "../Key";
 
 describe("<Key />", () => {
+    let wrapper = null;
+    const callback = sinon.spy();
+    const component =
+        <Key
+            text="btn1"
+            onClick={callback}
+            value="1"
+        />;
+
+    beforeEach(() => {
+        wrapper = shallow(component);
+    });
+
     context("when the component was successfully created", () => {
         it("renders without crashing", () => {
-            shallow(<Key text="btn1" />);
+            shallow(component);
         });
 
         it("renders the text from button", () => {
-            let wrapper = shallow(<Key text="btn1"/>);
             expect(wrapper).toHaveText('btn1');
+        });
+    });
+
+    context("when the component is clicked", () => {
+        it("calleds the callback", () => {
+            wrapper.find('button').simulate('click');
+
+            expect(callback.calledWith("1")).toBeTruthy();
         });
     });
 });
